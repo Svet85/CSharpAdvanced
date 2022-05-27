6657f07781e4ca652219bcde75f9bb7864bce671
svetlomirslavov <svetemilov@abv.bg>
2022-05-27 19:52:36 +0300

    lab exercises

---
+++ StreamsFilesDirectories/LineNumbers/LineNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new StreamReader(inputFilePath))
+            {
+
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    int counter = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StreamsFilesDirectories/WordCount/WordCount.cs
@@ -0,0 +1,61 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class WordCount
+    {
+        static void Main(string[] args)
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            using StreamReader reader = new StreamReader(wordsFilePath);
+            string[] words = reader.ReadToEnd().Split();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!dict.ContainsKey(words[i]))
+                {
+                    dict.Add(words[i], 0);
+                }
+            }
+
+            using (var reader_2 = new StreamReader(textFilePath))
+            {
+                while (!reader_2.EndOfStream)
+                {
+                    string line = reader_2.ReadLine().ToLower();
+
+                    foreach (var word in words)
+                    {
+                        Regex myRegex = new Regex($@"\b{word}\b");
+                        var results = myRegex.Matches(line);
+                        dict[word] += results.Count;
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in dict.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+
+
+        }
+    }
+}
