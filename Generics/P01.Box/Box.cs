6f3fd45f5f1a509d1c98a4b0a4a16398e88b3aac
svetlomirslavov <svetemilov@abv.bg>
2022-06-08 20:44:29 +0300

    lab exercises

---
+++ Generics/P01.Box/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            data = new List<T>();
+        }
+        public int Count => this.data.Count;
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Remove()
+        {
+            var removed = this.data.Last();
+            data.RemoveAt(data.Count - 1);
+            return removed;
+        }
+    }
+}
---
+++ Generics/P03.GenericScale/EqualiltyScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualiltyScale<T>
+    {
+        private T left;
+        private T right;
+        public EqualiltyScale(T left, T right)
+        {
+            this.right = right;
+            this.left = left;
+        }
+
+        public bool AreEqual()
+        {
+            bool result = this.left.Equals(this.right);
+            return result;
+        }
+    }
+}
