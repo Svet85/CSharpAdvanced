7e9663934f5d6cec1ec85744a6fcc41eef628347
svetlomirslavov <svetemilov@abv.bg>
2022-06-10 20:30:53 +0300

    lab exercises

---
+++ ImplementingStackAndQueue/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementingStackAndQueue
+{
+    public class MyList
+    {
+        private const int InitialCapacity = 4;
+        private int[] collection;
+        public MyList()
+        {
+            collection = new int[InitialCapacity];
+        }
+        public int Capacity => collection.Length;
+        public int Count { get; private set; }
+        public int this[int i]
+        {
+            get 
+            {
+                if (i < 0 || i >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return collection[i]; 
+            }
+            set 
+            {
+                if (i < 0 || i >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                collection[i] = value; 
+            }
+        }
+
+        public void AddElement(int element)
+        {
+            if (Count == Capacity - 2)
+            {
+                int[] resizedArray = new int[Capacity * 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    resizedArray[i] = collection[i];
+                }
+                collection = resizedArray;
+            }
+
+            collection[Count] = element;
+            Count++;
+        }
+
+        public int RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            Count--;
+
+            int removedElement = collection[index];
+            for (int i = index; i < Count; i++)
+            {
+                collection[i] = collection[i + 1];
+            }
+
+            return removedElement;
+        }
+    }
+}
+
---
+++ ImplementingStackAndQueue/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ImplementingStackAndQueue
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lilst = new MyList();
+            Console.WriteLine(lilst.Count);
+            Console.WriteLine(lilst.Capacity);
+            lilst.AddElement(0);
+            lilst.AddElement(0);
+            lilst.AddElement(0);
+            lilst.AddElement(0);
+            Console.WriteLine(lilst.Count);
+            Console.WriteLine(lilst.Capacity);
+
+        }
+    }
+}
