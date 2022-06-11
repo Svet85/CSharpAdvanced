63445946081bfbce933299f552e2adfbdad4a2d7
svetlomirslavov <svetemilov@abv.bg>
2022-06-11 07:53:02 +0300

    lab exercises

---
+++ Generics/P09.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box()
+        {
+
+        }
+        public Box(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+
+        public int CompareTo([AllowNull] T other)
+        {
+            return this.Value.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.Value}";
+        }
+    }
+}
---
+++ Generics/P09.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericCountMethodDouble
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Box<double>> elements = new List<Box<double>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var box = new Box<double>(double.Parse(Console.ReadLine()));
+                elements.Add(box);
+            }
+            double comparingElement = double.Parse(Console.ReadLine());
+            int count = NumberOfGreaterElements(elements, comparingElement);
+            Console.WriteLine(count);
+        }
+
+        public static int NumberOfGreaterElements<T>(List<Box<T>> elements, T comparingElement)
+            where T : IComparable
+        {
+            int result = 0;
+            foreach (var item in elements)
+            {
+                if (item.CompareTo(comparingElement) > 0)
+                {
+                    result++;
+                }
+                
+            }
+
+            return result;
+        }
+    }
+}
