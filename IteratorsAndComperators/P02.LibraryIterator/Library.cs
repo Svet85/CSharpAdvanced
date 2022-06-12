bcad9005f2ce0eddeaf2a05c189d8f4d03df7e11
svetlomirslavov <svetemilov@abv.bg>
2022-06-12 18:27:39 +0300

    lab exercises

---
+++ IteratorsAndComperators/P02.LibraryIterator/Library.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int currentIndex;
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (currentIndex < this.books.Count - 1)
+                {
+                    currentIndex++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+    }
+}
---
+++ IteratorsAndComperators/P03.ComparableBooks/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
