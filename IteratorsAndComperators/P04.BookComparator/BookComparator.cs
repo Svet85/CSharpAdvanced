18a2e93b88a8c00b7de3a0553428240dba7a0fa9
svetlomirslavov <svetemilov@abv.bg>
2022-06-13 18:24:35 +0300

    lab exercises

---
+++ IteratorsAndComperators/P04.BookComparator/BookComparator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            int result = x.Title.CompareTo(y.Title);
+            if (result == 0)
+            {
+                result = (x.Year * -1).CompareTo((y.Year * -1));
+            }
+
+            return result;
+        }
+    }
+}
