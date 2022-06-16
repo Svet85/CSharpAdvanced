679293b2c592b1edddcaee474945e471dc2c3913
svetlomirslavov <svetemilov@abv.bg>
2022-06-16 18:51:25 +0300

    lab exercises

---
+++ IteratorsAndComperators/P09.ComparingObjects/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] info = input.Split();
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                string town = info[2];
+
+                people.Add(new Person(name, age, town));
+
+                input = Console.ReadLine();
+            }
+
+            Person personToCompare = people[int.Parse(Console.ReadLine()) - 1];
+            int equalPeople = 0;
+            int differentPeople = 0;
+
+            foreach (var item in people)
+            {
+                if (item.CompareTo(personToCompare) == 0)
+                {
+                    equalPeople++;
+                }
+                else
+                {
+                    differentPeople++;
+                }
+            }
+
+            if (equalPeople == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalPeople} {differentPeople} {people.Count}");
+            }
+
+        }
+    }
+}
