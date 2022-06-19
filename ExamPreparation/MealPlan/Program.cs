142720b2f7105551574eab52b9391b53ef068a95
svetlomirslavov <svetemilov@abv.bg>
2022-06-19 08:25:16 +0300

    exam problems

---
+++ ExamPreparation/MealPlan/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MealPlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> meals = new Queue<string>(Console.ReadLine().Split());
+            Stack<int> dailyCalories = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int mealsStartingCount = meals.Count;
+
+            while (meals.Count > 0 && dailyCalories.Count > 0)
+            {
+                int mealCalories = MealCalories(meals.Peek());
+                int daily = dailyCalories.Peek();
+
+                if (daily - mealCalories == 0)
+                {
+                    meals.Dequeue();
+                    dailyCalories.Pop();
+                }
+                else if (daily - mealCalories > 0)
+                {
+                    int leftDaily = daily - mealCalories;
+                    meals.Dequeue();
+                    dailyCalories.Pop();
+                    dailyCalories.Push(leftDaily);
+                }
+                else if (daily - mealCalories < 0)
+                {
+                    int mealLeft = mealCalories - daily;
+                    meals.Dequeue();
+                    dailyCalories.Pop();
+                    if (dailyCalories.Count > 0)
+                    {
+                        mealCalories = dailyCalories.Pop() - mealLeft;
+                        dailyCalories.Push(mealCalories);
+                    }
+                }
+            }
+
+            if (dailyCalories.Count == 0)
+            {
+                Console.WriteLine($"John ate enough, he had {mealsStartingCount - meals.Count} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+            }
+            else
+            {
+                Console.WriteLine($"John had {mealsStartingCount} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", dailyCalories)} calories.");
+            }
+        }
+
+        public static int MealCalories(string meal)
+        {
+            int calories = 0;
+            switch (meal)
+            {
+                case "salad":
+                    calories = 350;
+                    break;
+                case "soup":
+                    calories = 490;
+                    break;
+                case "pasta":
+                    calories = 680;
+                    break;
+                case "steak":
+                    calories = 790;
+                    break;
+                default:
+                    break;
+            }
+
+            return calories;
+        }
+    }
+}
---
+++ ExamPreparation/Zoo/Zoo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+        public Zoo(string name, int capacity)
+        {
+            Animals = new List<Animal>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public List<Animal> Animals { get; private set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public string AddAnimal(Animal animal)
+        {
+            if (string.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+
+            if (!(animal.Diet == "herbivore" || animal.Diet == "carnivore"))
+            {
+                return "Invalid animal diet.";
+            }
+
+            if (Animals.Count == Capacity)
+            {
+                return "The zoo is full.";
+            }
+
+            Animals.Add(animal);
+            return $"Successfully added {animal.Species} to the zoo.";
+        }
+
+        public int RemoveAnimals(string species)
+        {
+            int count = Animals.RemoveAll(x => x.Species == species);
+            return count;
+        }
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            var animalsbyDiet = Animals.Where(x => x.Diet == diet).ToList();
+            return animalsbyDiet;
+        }
+
+        public Animal GetAnimalByWeight(double weight)
+        {
+            var animalByWeight = Animals.FirstOrDefault(x => x.Weight == weight);
+            return animalByWeight;
+        }
+
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            int count = Animals.Count(x => x.Length >= minimumLength && x.Length <= maximumLength);
+            return $"There are {count} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+    }
+}
