0b5a14c8faa4bd19cbfa832a30582eaf34eb6a72
svetlomirslavov <svetemilov@abv.bg>
2022-06-23 18:20:35 +0300

    exam problems

---
+++ ExamPreparation/Armory/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace Armory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] armory = new char[size, size];
+
+            for (int r = 0; r < size; r++)
+            {
+                char[] array = Console.ReadLine().ToCharArray();
+                for (int c = 0; c < size; c++)
+                {
+                    armory[r, c] = array[c];
+                }
+            }
+            int goldSpent = 0;
+
+            int[] officer = FindOfficer(armory, size);
+            int officerRow = officer[0];
+            int officerCol = officer[1];
+
+
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                
+                armory[officerRow, officerCol] = '-';
+
+                if (cmd == "up")
+                {
+                    if (officerRow == 0)
+                    {
+                        Console.WriteLine("I do not need more swords!");
+                        break;
+                    }
+
+                    officerRow--;
+
+                    if (char.IsDigit(armory[officerRow,officerCol]))
+                    {
+                        goldSpent += armory[officerRow, officerCol] -'0';
+                    }
+                    else if (armory[officerRow, officerCol] == 'M')
+                    {
+                        armory[officerRow, officerCol] = '-';
+                        int[] otherMirror = OtherMirrorCoordinates(armory,size);
+                        officerRow = otherMirror[0];
+                        officerCol = otherMirror[1];
+                    }
+
+                }
+                else if (cmd == "down")
+                {
+                    if (officerRow == size - 1)
+                    {
+                        Console.WriteLine("I do not need more swords!");
+                        break;
+                    }
+
+                    officerRow++;
+
+                    if (char.IsDigit(armory[officerRow, officerCol]))
+                    {
+                        goldSpent += armory[officerRow, officerCol] - '0';
+                    }
+                    else if (armory[officerRow, officerCol] == 'M')
+                    {
+                        armory[officerRow, officerCol] = '-';
+                        int[] otherMirror = OtherMirrorCoordinates(armory, size);
+                        officerRow = otherMirror[0];
+                        officerCol = otherMirror[1];
+                    }
+                }
+                else if (cmd == "left")
+                {
+                    if (officerCol == 0)
+                    {
+                        Console.WriteLine("I do not need more swords!");
+                        break;
+                    }
+
+                    officerCol--;
+
+                    if (char.IsDigit(armory[officerRow, officerCol]))
+                    {
+                        goldSpent += armory[officerRow, officerCol] - '0';
+                    }
+                    else if (armory[officerRow, officerCol] == 'M')
+                    {
+                        armory[officerRow, officerCol] = '-';
+                        int[] otherMirror = OtherMirrorCoordinates(armory, size);
+                        officerRow = otherMirror[0];
+                        officerCol = otherMirror[1];
+                    }
+                }
+                else if (cmd == "right")
+                {
+                    if (officerCol == size - 1)
+                    {
+                        Console.WriteLine("I do not need more swords!");
+                        break;
+                    }
+
+                    officerCol++;
+
+                    if (char.IsDigit(armory[officerRow, officerCol]))
+                    {
+                        goldSpent += armory[officerRow, officerCol] - '0';
+                    }
+                    else if (armory[officerRow, officerCol] == 'M')
+                    {
+                        armory[officerRow, officerCol] = '-';
+                        int[] otherMirror = OtherMirrorCoordinates(armory, size);
+                        officerRow = otherMirror[0];
+                        officerCol = otherMirror[1];
+                    }
+                }
+
+                armory[officerRow, officerCol] = 'A';
+                
+                if (goldSpent >= 65)
+                {
+                    Console.WriteLine("Very nice swords, I will come back for more!");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"The king paid {goldSpent} gold coins.");
+            for (int r = 0; r < size; r++)
+            {
+                for (int c = 0; c < size; c++)
+                {
+                    Console.Write(armory[r, c]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public static int[] OtherMirrorCoordinates(char[,] armory, int size)
+        {
+            int[] array = new int[2];
+            bool flag = false;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    if (armory[i, k] == 'M')
+                    {
+                        array[0] = i;
+                        array[1] = k;
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (flag)
+                {
+                    break;
+                }
+            }
+
+            return array;
+
+
+        }
+
+        public static int[] FindOfficer(char[,] armory, int size)
+        {
+            int[] array = new int[2];
+            bool flag = false;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    if (armory[i, k] == 'A')
+                    {
+                        array[0] = i;
+                        array[1] = k;
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (flag)
+                {
+                    break;
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ ExamPreparation/Blacksmith/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blacksmith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> steel = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> carbon = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Dictionary<string, int> swords = new Dictionary<string, int>()
+            {
+                ["Gladius"] = 0,
+                ["Shamshir"] = 0,
+                ["Katana"] = 0,
+                ["Sabre"] = 0,
+                ["Broadsword"] = 0,
+            };
+
+            while (steel.Count > 0 && carbon.Count > 0)
+            {
+                int currentSteel = steel.Dequeue();
+                int currentCarbon = carbon.Pop();
+                string swordType = Forging(currentSteel,currentCarbon);
+
+                if (swordType == "invalid")
+                {
+                    currentCarbon += 5;
+                    carbon.Push(currentCarbon);
+                }
+                else
+                {
+                    swords[swordType]++;
+                }
+            }
+
+            if (swords.Values.Sum() > 0)
+            {
+                Console.WriteLine($"You have forged {swords.Values.Sum()} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+
+            string leftSteel = steel.Count > 0 ? string.Join(", ", steel) : "none";
+            string leftCarbon = carbon.Count > 0 ? string.Join(", ", carbon) : "none";
+            Console.WriteLine($"Steel left: {leftSteel}");
+            Console.WriteLine($"Carbon left: {leftCarbon}");
+
+            foreach (var sword in swords.Where(x => x.Value > 0).OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{sword.Key}: {sword.Value}");
+            }
+        }
+
+        public static string Forging(int currentSteel, int currentCarbon)
+        {
+            int sum = currentCarbon + currentSteel;
+            string sword = "";
+
+            switch (sum)
+            {
+                case 70:
+                    sword = "Gladius";
+                    break;
+                case 80:
+                    sword = "Shamshir";
+                    break;
+                case 90:
+                    sword = "Katana";
+                    break;
+                case 110:
+                    sword = "Sabre";
+                    break;
+                case 150:
+                    sword = "Broadsword";
+                    break;
+                default:
+                    sword = "invalid";
+                    break;
+            }
+
+            return sword;
+        }
+    }
+}
